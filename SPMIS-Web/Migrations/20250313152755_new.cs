6f70a564ef7fc55c252b1c7f9c9b5754447ea2e0
Marion <marion.penalosa.cics@ust.edu.ph>
2025-03-13 23:35:04 +0800

    Merge w Kyle: add objective modal working, POST not working

---
+++ SPMIS-Web/Migrations/20250313152755_new.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPMIS_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class @new : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "StrategyMapMapId",
+                table: "ObjectiveTypes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectiveTypes_StrategyMapMapId",
+                table: "ObjectiveTypes",
+                column: "StrategyMapMapId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ObjectiveTypes_StrategyMaps_StrategyMapMapId",
+                table: "ObjectiveTypes",
+                column: "StrategyMapMapId",
+                principalTable: "StrategyMaps",
+                principalColumn: "MapId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ObjectiveTypes_StrategyMaps_StrategyMapMapId",
+                table: "ObjectiveTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ObjectiveTypes_StrategyMapMapId",
+                table: "ObjectiveTypes");
+
+            migrationBuilder.DropColumn(
+                name: "StrategyMapMapId",
+                table: "ObjectiveTypes");
+        }
+    }
+}
