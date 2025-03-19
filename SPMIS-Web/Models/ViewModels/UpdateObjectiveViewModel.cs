2630d42f1870b4be06a7e3c09e7ea1ab4833fe31
ppaattxx <138113186+ppaattxx@users.noreply.github.com>
2025-03-19 12:49:56 +0800

    Reapply "UPDATED: UI, Edit Map"
    This reverts commit cc3317a635252ab0359b3af3bc1f8cf23fa77664.

---
+++ SPMIS-Web/Models/ViewModels/UpdateObjectiveViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SPMIS_Web.Models.Entities;
+
+namespace SPMIS_Web.Models.ViewModels
+{
+    public class UpdateObjectiveViewModel
+    {
+        public Guid ObjectiveId { get; set; }
+        public string ObjectiveDescription { get; set; }
+
+        //public Guid ObjectiveTypeId { get; set; }
+        public List<ObjectiveType> ObjectiveType { get; set; } = new List<ObjectiveType>();
+        public Guid ObjectiveTypeId { get; set; }
+
+        public Guid MapId { get; set; }
+    }
+}
