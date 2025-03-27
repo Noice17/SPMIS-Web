8116ef952b57eb71bf15c07d84940273dd212573
Marion <marion.penalosa.cics@ust.edu.ph>
2025-03-27 09:39:18 +0800

    Commitment Form

---
+++ SPMIS-Web/Controllers/CommitmentFormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPMIS_Web.Controllers
+{
+    public class CommitmentFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
